33c5df70cf9a1aea743b3e006ae1d803374e4549
Khalid Alhumaidan <khalid.ib21@gmail.com>
2023-04-10 22:34:04 +0300

    adding yellow light

--- Main.py
+++ Main.py
@@ -8,13 +8,14 @@ trafficLights = [(0,0),
                  (3,0)]
 
 def main():
-    dt = ObjDetection.ObjDetection()
-    model = dt.loadModel(0.60)
-    while True:
+    dt = ObjDetection.ObjDetection() #create object detection object
+    model = dt.loadModel(0.60) #load model with confidence threshold of 60%
+
+    while True: #infinite loop to keep the traffic light on
         updateLights(dt, model)
 
 
-def updateLights(dt, model):
+def updateLights(dt, model): #update the traffic lights times
     for i in range(4):
         sec, light = dt.detectObject(model,i)
         if light == 0:
@@ -28,25 +29,29 @@ def updateLights(dt, model):
     #print(trafficLights)
     trafficCycle(dt, model)
 
-def trafficCycle(dt, model):
+def trafficCycle(dt, model): #traffic light cycle
     for i in range(4):
         sec = trafficLights[i][1]
-        if sec == 0:
-            print(f'no cars detected in light {i}, skipping this light')
+
+        if sec == 0: #check if there are no cars at the light
+            print(f'no cars detected in light {i}, skipping this light...') 
             continue
+
         print(f'cars detected at light {i}')
-        for j in range(sec,0,-1):
+
+        for j in range(sec,0,-1): #traffic light ON
             sleep(1)
             sec -= 1
             print(f'light {i} is green for {j} seconds')
-            secToOff,_ = dt.detectObject(model,i)
-            if secToOff == 0:
-                print(f'no cars, turning off the light {i}')
+            secToOff,_ = dt.detectObject(model,i) #to check if there are cars while the light is ON
+
+            if secToOff == 0 or sec == 0: #if there are no cars or the time is up, turn the light to yellow
+                print(f'no cars at light {i}, turning Yellow light...')
+                for i in range(3):
+                    sleep(1)
+                    print(f'light {i} is yellow for {3-i} seconds')
                 sec = 0
                 break
-
-
-
 
 if __name__ == '__main__':
     main()
